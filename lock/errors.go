6ac9fd10b14a2c634794a985b085b209b090488f
Vishnu Varthan <vishnulatha006@gmail.com>
2025-01-24 15:51:10 +0530

    implement release

--- lock/errors.go
+++ lock/errors.go
@@ -4,5 +4,6 @@ import "errors"
 
 var (
 	ErrUnableToAcquireLock       = errors.New("unable to acquire lock")
+	ErrUnableToReleaseLock       = errors.New("unable to release lock")
 	ErrContextWithDeadlineNeeded = errors.New("context with deadline needed,Refer more https://tinyurl.com/58ccxyey")
 )
--- lock/red_lock.go
+++ lock/red_lock.go
@@ -65,7 +65,9 @@ func (r *redLock) Acquire(ctx context.Context, key string) error {
 
 	for _, c := range r.clients {
 		wg.Add(1)
-		go func() {
+		// Practically finite number of redis clusters which is acceptable count to make concurrent requests
+		// will only will be there, just adding semaphore to gracefully handle the situation
+		go func(c *redis.Client) {
 			defer wg.Done()
 			<-r.requestSem
 			// After the sem is acquired release it after the operation is done
@@ -78,7 +80,7 @@ func (r *redLock) Acquire(ctx context.Context, key string) error {
 				acquired: acquiredResult.Val(),
 			}
 			r.requestSem <- struct{}{}
-		}()
+		}(c)
 
 	}
 
@@ -110,5 +112,61 @@ func (r *redLock) Acquire(ctx context.Context, key string) error {
 
 // Release implements ILock.
 func (r *redLock) Release(ctx context.Context, key string) error {
-	panic("unimplemented")
+	var (
+		releasedCount = atomic.Int32{}
+		quorum        = len(r.clients)/2 + 1
+		done          = make(chan interface{})
+		wg            sync.WaitGroup
+	)
+
+	_, ok := ctx.Deadline()
+	if !ok {
+		logrus.Error(ErrContextWithDeadlineNeeded.Error())
+		return ErrContextWithDeadlineNeeded
+	}
+
+	for _, c := range r.clients {
+		wg.Add(1)
+		go func(client *redis.Client) {
+			defer wg.Done()
+			<-r.requestSem
+			// After the sem is acquired release it after the operation is done
+			if releasedCount.Load() == int32(quorum) {
+				close(done)
+				return
+			}
+			result := client.Del(ctx, key)
+			if result.Val() == 1 {
+				if releasedCount.Add(1) == int32(quorum) {
+					close(done)
+					return
+				}
+			}
+			r.requestSem <- struct{}{}
+		}(c)
+	}
+
+	go func() {
+		wg.Wait()
+		select {
+		case <-ctx.Done():
+			return
+		case <-done:
+			return
+		default:
+			close(done)
+		}
+	}()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case <-done:
+			if releasedCount.Load() == int32(quorum) {
+				return nil
+			}
+			return ErrUnableToReleaseLock
+		}
+	}
 }
